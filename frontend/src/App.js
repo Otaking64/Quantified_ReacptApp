d57c2ee4318382b849a9f1ef148dd701b783479c
QuentenZ <quentenzandvliet@me.com>
2019-12-18 01:26:05 +0100

    Headbar in app.js
    Deze is op iedere pagina te zien en is een headbar met simpel menu (niet af)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,42 @@
 import React, { useState, useEffect } from "react";
 import { Link, withRouter } from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
-import { Menu as MenuIcon } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, IconButton, FormControlLabel, FormGroup, MenuItem, Menu } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 import { LinkContainer } from "react-router-bootstrap";
 import { Auth } from "aws-amplify";
 import "./App.css";
 import Routes from "./Routes";
 
 const useStyles = makeStyles(theme => ({
-  menuButton: {
-    marginRight: theme.spacing(2),
+  root: {
+    flexGrow: 1,
   },
   title: {
     flexGrow: 1,
-    align:'center',
-    fontSize: 20,
   },
-}))
+}));
 
 function App(props) {
   const [isAuthenticated, userHasAuthenticated] = useState(false);
   const [isAuthenticating, setIsAuthenticating] = useState(true);
   const classes = useStyles();
+  const [setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleChange = event => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   useEffect(() => {
     onLoad();
@@ -52,33 +66,57 @@ function App(props) {
 
   return (
   !isAuthenticating &&
-  <div className="App container">
-  <AppBar position="static">
-    <Toolbar>
-      <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-        <MenuIcon />
-      </IconButton>
-      <Typography variant="h6" className={classes.title}>
-        Home
-      </Typography>
-      {isAuthenticated
-      ? <Button color="inherit" onClick={handleLogout}>Login</Button>
-      : <>
-      <LinkContainer to="/signup">
-        <Button color="inherit">Signup</Button>
-      </LinkContainer>
-      <LinkContainer to="/login">
-        <Button color="inherit">Login</Button>
-      </LinkContainer>
-      <LinkContainer to="/">
-        <Button color="inherit">Home</Button>
-      </LinkContainer>
-      </>
-    }
-    </Toolbar>
-  </AppBar>
-    <Routes appProps={{ isAuthenticated, userHasAuthenticated }} />
-  </div>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Home
+          </Typography>
+          {isAuthenticated
+          ? <IconButton color="inherit" onClick={handleLogout}>Login</IconButton>
+          : <>
+          <div>
+            <IconButton
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={open}
+              onClose={handleClose}
+            >
+              <LinkContainer to="/signup">
+                <MenuItem onClick={handleClose}>Signup</MenuItem>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <MenuItem onClick={handleClose}>Login</MenuItem>
+              </LinkContainer>
+              <LinkContainer to="/">
+                <MenuItem onClick={handleClose}>Home</MenuItem>
+              </LinkContainer>
+            </Menu>
+          </div>
+          </>
+        }
+        </Toolbar>
+      </AppBar>
+      <Routes appProps={{ isAuthenticated, userHasAuthenticated }} />
+    </div>
   );
 }
 

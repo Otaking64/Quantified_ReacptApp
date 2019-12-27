7ad8d577f2b5e40d4099b75c7e43040a087fa77f
Mark Heij <mark.heij.97@gmail.com>
2019-12-27 16:53:32 +0100

    Step 2 uitgewerkt en icons toegevoegd
    Volgende dingen gedaan:
    Stap 2 (items in doos checken en icons uitleggen) uitgewerkt
    
    To-Do: Node icon toevoegen

--- frontend/src/steps/Step_2_Box.js
+++ frontend/src/steps/Step_2_Box.js
@@ -1,76 +1,88 @@
 import React from "react";
 import "../containers/Home.css";
 import {
+  Avatar,
   Button,
-  Checkbox,
-  FormLabel,
-  FormHelperText,
-  FormGroup,
-  FormControlLabel,
-  FormControl,
   Typography,
-  Container,
   Grid,
   Box,
   LinearProgress,
   makeStyles,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
 } from '@material-ui/core';
-import { LinkContainer } from "react-router-bootstrap"
-import {red} from '@material-ui/core/colors'
-import RouterImage from '../img/router.jpg'
-import HelpIcon from '@material-ui/icons/Help';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Dialog from '@material-ui/core/Dialog';
+import { LinkContainer } from "react-router-bootstrap";
+import PowerIcon from '@material-ui/icons/Power';
+import RouterIcon from '@material-ui/icons/Router';
+import SettingsEthernetIcon from '@material-ui/icons/SettingsEthernet';
 
 const useStyles = makeStyles(theme => ({
   button: {
     bottom: 20,
     position: 'fixed',
     margin: 0,
-  }
+  },
+  avatarColor:{
+    backgroundColor:'#fff',
+  },
 }))
 
 export default function Step2(){
   const classes = useStyles()
   return(
-    <Container maxWidth="sm">
-      <Grid container alignItems="center" justify="center" direction="column">
-        <Grid item sm={12}>
-          <Box width={300} align="center">
-            <Typography variant="overline">Progress</Typography>
-            <LinearProgress variant="determinate" value={66}/>
-          </Box>
-        </Grid>
-        <Grid item xs={12}>
-            <img src={RouterImage}
-                 width="320w"
-                 />
-          <Typography variant="h4">Gateway</Typography>
-          <Typography variant="body1">Are the following items in the box?</Typography>
-          <FormGroup column text-align="left">
-            <FormControlLabel
-              control={
-                <Checkbox
-                  value="checkedA"
-                  inputProps={{ 'aria-label': 'Checkbox A' }}
-                  color="primary"
-                />
-              }
-                label="Gateway"
-            />
-            <FormControlLabel
-            control={
-              <Checkbox
-                value="checkedA"
-                inputProps={{ 'aria-label': 'Checkbox A' }}
-                color="primary"
-              />
-            }
-              label="Requirement 2"
-          />
-          </FormGroup>
+        <Grid container alignItems="center" justify="center" direction="column" spacing={2}>
+          <Grid item>
+            <Box width={300} align="center">
+              <Typography variant="overline">Progress</Typography>
+              <LinearProgress variant="determinate" value={66}/>
+            </Box>
           </Grid>
-          <Grid item xs={12} className={classes.button} align="">
+          <Grid item>
+            <Typography variant="h4" align="center">Open the box</Typography>
+            <Typography display="block" variant="body1">
+            In the box are the following items:
+            </Typography>
+          </Grid>
+          <Grid item>
+            <List>
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar className={classes.avatarColor}>
+                    <RouterIcon color="primary" size="large"/>
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary="The Gateway" secondary="The connection between the Nodes and the Internet" />
+              </ListItem>
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar className={classes.avatarColor}>
+                    <RouterIcon color="primary" />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary="Nodes" secondary="The sensors that give you insights" />
+              </ListItem>
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar className={classes.avatarColor}>
+                    <SettingsEthernetIcon color="primary"/>
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary="LAN cable" secondary="To connect the Gateway to the Internet" />
+              </ListItem>
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar className={classes.avatarColor}>
+                    <PowerIcon color="primary"/>
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary="Power Supply" secondary="To give the Gateway power" />
+              </ListItem>
+            </List>
+          </Grid>
+
+          <Grid item className={classes.button} align="center">
             <LinkContainer to="/step3">
               <Box pt={1}>
                 <Button size="large" variant="containedSecondary" color="primary">DONE</Button>
@@ -81,8 +93,7 @@ export default function Step2(){
                 <Button size="small" color="primary">Previous step</Button>
               </Box>
             </LinkContainer>
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
   );
 }

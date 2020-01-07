c62e8bdb387a4b610828187092351617d8483257
awestendorp <s1083928@student.hsleiden.nl>
2020-01-07 18:13:04 +0100

    maps, qr code and counter added

--- frontend/src/steps/Step_6_Node_Amount.js
+++ frontend/src/steps/Step_6_Node_Amount.js
@@ -1,24 +1,55 @@
 import React from "react";
 import "../containers/Home.css";
 import {
-  Container,
-  Typography,
-  Grid,
+    Button,
+    Container,
+    Typography,
+    Grid,
+    Box,
+    LinearProgress,
+    makeStyles, Avatar,
 } from '@material-ui/core';
+import { LinkContainer } from "react-router-bootstrap"
 import TopMenuBar from "../components/TopMenuBar";
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
+import NodeIcon from "../icons/icon_node";
+
+const useStyles = makeStyles(theme => ({
+    avatarColor:{
+        backgroundColor:'#fff',
+    },
+}))
+
+let nodequant = 1;
+
+const quanthandler = (direction) => {
+    if(direction === "up"){
+        nodequant += 1;
+    }else{
+        nodequant -= 1;
+    }
+}
 
 export default function Step6(){
-  return(
-    <Container>
-      <TopMenuBar block pageName="Installation" hamburgerMenu={false} closeButtonOnly={false} closeWithPrompt={true} backButton={false} backRoutePage="/"/>
-      <Grid container direction="column" alignItems="center" justify= "center" spacing={2}>
-        <Grid item>
-          <Typography variant="h4" align="center">Nodes</Typography>
-          <Typography display="block" variant="body1">
-          Choose the amount of nodes you have
-          </Typography>
-        </Grid>
-      </Grid>
-    </Container>
-  );
+    const classes = useStyles()
+
+    return(
+        <Container>
+            <TopMenuBar block pageName="Installation" hamburgerMenu={false} closeButtonOnly={false} closeWithPrompt={true} backButton={false} backRoutePage="/"/>
+            <Grid container direction="column" alignItems="center" justify= "center" spacing={2}>
+                <Grid item>
+                    <Typography variant="h4" align="center">Nodes</Typography>
+                    <Typography display="block" variant="body1">
+                        Choose the amount of nodes you have
+                    </Typography>
+                </Grid>
+                <Grid item>
+
+                    <input type="number" value={nodequant} name="quantity" min="1"  />
+
+                </Grid>
+            </Grid>
+        </Container>
+    );
 }

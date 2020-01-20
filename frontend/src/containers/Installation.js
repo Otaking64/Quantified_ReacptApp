9507a049229fac550e16c199577f1773945b2e9e
awestendorp <s1083928@student.hsleiden.nl>
2020-01-20 15:50:29 +0100

    Pascal case > Camel case

--- frontend/src/containers/Installation.js
+++ frontend/src/containers/Installation.js
@@ -5,16 +5,16 @@ import { Container, MobileStepper, Button } from '@material-ui/core';
 import { KeyboardArrowLeft, KeyboardArrowRight } from '@material-ui/icons';
 import TopMenuBar from "../components/TopMenuBar";
 
-import Step_1_Welcome from "../steps/Step_1_Welcome";
-import Step_2_Box from "../steps/Step_2_Box";
-import Step_3_Gateway from "../steps/Step_3_Gateway";
-import Step_4_Gateway_PowerSupply from "../steps/Step_4_Gateway_PowerSupply";
-import Step_5_Gateway_LAN from "../steps/Step_5_Gateway_LAN";
-import Step_6_Node_Amount from "../steps/Step_6_Node_Amount";
-import Step_7_Node_Grid from "../steps/Step_7_Node_Grid";
-import Step_8_Node_Add from "../steps/Step_8_Node_Add";
-import Step_9_Node_PositionMount from "../steps/Step_9_Node_PositionMount";
-import Step_10_End from "../steps/Step_10_End";
+import step_1_Welcome from "../steps/Step_1_Welcome";
+import step_2_Box from "../steps/Step_2_Box";
+import step_3_Gateway from "../steps/Step_3_Gateway";
+import step_4_Gateway_PowerSupply from "../steps/Step_4_Gateway_PowerSupply";
+import step_5_Gateway_LAN from "../steps/Step_5_Gateway_LAN";
+import step_6_Node_Amount from "../steps/Step_6_Node_Amount";
+import step_7_Node_Grid from "../steps/Step_7_Node_Grid";
+import step_8_Node_Add from "../steps/Step_8_Node_Add";
+import step_9_Node_PositionMount from "../steps/Step_9_Node_PositionMount";
+import step_10_End from "../steps/Step_10_End";
 import firebase from "firebase";
 
 const useStyles = makeStyles(theme => ({
@@ -41,7 +41,7 @@ export default function Installation(props) {
       props.history.push("/login")
     }
   });
-  const steps = [<Step_1_Welcome/>, <Step_2_Box/>, <Step_3_Gateway/>, <Step_4_Gateway_PowerSupply/>, <Step_5_Gateway_LAN/>, <Step_6_Node_Amount/>, <Step_7_Node_Grid/>, <Step_8_Node_Add/>, <Step_9_Node_PositionMount/>, <Step_10_End/>];
+  const steps = [<step_1_Welcome/>, <step_2_Box/>, <step_3_Gateway/>, <step_4_Gateway_PowerSupply/>, <step_5_Gateway_LAN/>, <step_6_Node_Amount/>, <step_7_Node_Grid/>, <step_8_Node_Add/>, <step_9_Node_PositionMount/>, <step_10_End/>];
 
   const handleNext = () => {
     setActiveStep(prevActiveStep => prevActiveStep + 1);
